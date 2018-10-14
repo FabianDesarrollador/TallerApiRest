cf8e505a5801a6a426163904036459e4170404ca
Fabián Andrés Cano Hernández <desarrolladorsistemas@outlook.com>
2018-10-14 17:35:43 -0500

    Modelo Inicial

---
+++ TallerWebService/Controllers/PublicacionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TallerWebService.Models;
+
+namespace TallerWebService.Controllers
+{
+    public class PublicacionController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Publicacion> Get()
+        {
+            using (var context = new PublicacionContext())
+            {
+                return context.Publicaciones.ToList();
+            }
+        }
+    }
+}
